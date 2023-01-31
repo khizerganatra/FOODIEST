32e79dd650dcc2c1213d89660cda1848d8c005d1
khizerganatra <112175403+khizerganatra@users.noreply.github.com>
2023-01-31 07:08:38 +0500

    uploaded new website

---
+++ javascript/index.js
@@ -0,0 +1,20 @@
+// var percentage = prompt("WRITE YOUR PERCENTAGE (EXAMPLE 89, 72... etc without entering % sign)")
+
+// if(percentage >= 90){
+//     console.log("YOUR GRADE IS A*")
+// }
+// else if (percentage >= 80){
+//     console.log("YOUR GRADE IS A")
+// }
+// else if (percentage >= 70){
+//     console.log("YOUR GRADE IS B")
+// }
+// else if (percentage >= 60){
+//     console.log("YOUR GRADE IS C")
+// }
+// else if (percentage >= 50){
+//     console.log("YOUR GRADE IS D")
+// }
+// else if (percentage < 50){
+//     console.log("YOUR GRADE IS F")
+// }
